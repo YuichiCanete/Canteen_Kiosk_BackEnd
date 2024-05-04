b8286c5780142c16a8479e74956dbed1d4623144
YuichiCanete <142189950+YuichiCanete@users.noreply.github.com>
2024-05-04 15:42:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.include_router(viewOrderRouter, prefix="/api")
 # Configure CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
